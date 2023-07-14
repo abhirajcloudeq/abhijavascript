c39cf49bf78512586b86073542b3f19ad03c0539
Abhiraj Chaudhary <133480538+abhirajcloudeq@users.noreply.github.com>
2023-07-14 12:30:31 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,22 @@
+// let hour =10;
+// let weekday = true;
+
+// if {
+
+//     ((hour<=10 || hour>=15) && weekday))
+//     alert("weekend")
+// }
+ num1=12;
+ num2=20;
+ num3=4;
+ num4=5;
+ 
+ console.log(num1);
+ console.log(num2);
+ console.log(num3);
+ console.log(num4);
+
+ console.log(num1 > num2);
+ console.log(num2 <= num3);
+ console.log(num3 == num4);
+ console.log(num3 != num1);
